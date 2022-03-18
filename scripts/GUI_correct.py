f3e86144ac01da0b37d4c8a84a7de1843bb5b755
Victor Ibañez <76455152+victorjonathanibanez@users.noreply.github.com>
2022-03-18 12:15:17 +0100

    Update GUI_correct.py

--- scripts/GUI_correct.py
+++ scripts/GUI_correct.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # -------------------------------------------------------------------------------------
-# ORT analysis - script GUI prediction - developed by Victor Ibañez
+# ORT analysis - script GUI correct - developed by Victor Ibañez
 # 03.04.2021
 # -------------------------------------------------------------------------------------
 
@@ -19,18 +19,22 @@ import os
 # -----------------------------------------------------
 
 def GUI_correct():
-
+	
+	# create main interface
 	def call_dic(dic):
-
+		
+		# grab dictionary structure 
 		for key, value in dic.items():
 			
 			for i in range(len(value)):
-
+				
+				# create labels
 				if value[i][0] == 'Label':
 					lbl=Label(window, text=value[i][1][1])
 					lbl.place(x=int(win_w*value[i][1][0]), y=int(win_h*key))
 					lbl.config(font=("TkDefaultFont", f_size, "italic"))
-
+				
+				# create buttons
 				if value[i][0] == 'Button':
 					if value[i][1][1] == 'path':
 						btn=Button(window, text="search", command=open_path)
@@ -43,30 +47,39 @@ def GUI_correct():
 
 					btn.place(x=int(win_w*value[i][1][0]), y=int(win_h*key))
 					btn.config(font=("TkDefaultFont", f_size, "italic"))
-
+	
+	# function to get project path
 	def open_path():
 		global project_path
 		project_path = filedialog.askdirectory()
+		
+		# add label below the button
 		lbl6=Label(window, text=project_path.ljust(1000))
 		lbl6.place(x=int(win_w*0.05), y=int(win_h*0.13))
 		lbl6.config(font=("Arial", f_size-2))
-
+	
+	# function to get source of videos path
 	def source_path():
 		global source
 		source = filedialog.askdirectory()
+		
+		# add label below the button
 		lbl7=Label(window, text=os.path.basename(source).ljust(1000))
 		lbl7.place(x=int(win_w*0.05), y=int(win_h*0.48))
 		lbl7.config(font=("Arial", f_size-2))
-
+	
+	# function to get video paths
 	def open_vid():
 		global videos
 
 		videos = fd.askopenfilenames()
-
+		
+		# take first video and get basename, then calculate the nb. of video name strings fits into one horizontal line of the GUI
 		name0 = os.path.basename(videos[0]).split('.')[0]
 		l = len(name0)
 		nb_items = int((win_w/(l+2))/(f_size/1.5))
-
+		
+		# add label below button in several rows if necessary
 		for i in range(len(videos)):
 			name = os.path.basename(videos[i]).split('.')[0]
 
@@ -81,25 +94,34 @@ def GUI_correct():
 				lbl3=Label(window, text=name.ljust(1000))
 				lbl3.place(x=int(win_w*0.05)+((i-nb_items)*int(win_w*0.05)), y=int(win_h*0.31))
 				lbl3.config(font=("Arial", f_size-2))
-
+	
+	# start analysis
 	def submit():
 		#global project_path
 		#global source
 		window.destroy()
 
 	
+	# ------------------------------------------------------------------------------------------------------
+	# 				*** MAIN SETTINGS ***
+	# ------------------------------------------------------------------------------------------------------
+	
 	window=Tk()
-
+	
+	# get screen dimensions info
 	width = window.winfo_screenwidth()
 	height = window.winfo_screenheight()
 	win_w = int(width/2)
 	win_h = int(height/1.15)
-
+	
+	# default font size compared to screen dimensions
 	norm_w = 1280
 	norm_f_size = 14
-
+	
+	# calculate fontsize depending on different screen dimensions than default 
 	f_size = round((norm_w*norm_f_size)/width)
 	
+	# main structure of GUI
 	main = {0.03:[['Label',[0.05,'choose project path:']]],
 			0.08:[['Button',[0.05,'path']]],
 			0.18:[['Label',[0.05,'choose prediction videos for correction:']]],
@@ -108,9 +130,10 @@ def GUI_correct():
 			0.43:[['Button',[0.05,'vid_path']]],
 			0.94:[['Button',[0.05,'submit']]]}
 				
-
+	# create list to grab entries of main 
 	entries = []
 	
+	# call main function
 	call_dic(main)
 
 	window.title('Create a new Project!')
